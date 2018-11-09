202339a1439f233290da9008d8f3eeb3033439b6
gauravrajminhas <gaurav.minhas@gmail.com>
2018-11-09 14:26:56 -0500

    adding more Pocos Classe  or Entity Class def

--- CareerCloud.Pocos/ApplicantProfilePoco.cs
+++ CareerCloud.Pocos/ApplicantProfilePoco.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CareerCloud.Pocos
 {
+    [Table("Applicant_Profiles")]
     class ApplicantProfilePoco : IPoco
     {
         [Key]
         public Guid Id { get; set; }
+
+        public Guid Login { get; set; }
+
+        [Column("Current_Salary")]
+        public Decimal? CurrentSalary { get; set; }
+
+        [Column("Current_Rate")]
+        public Decimal? CurrentRate { get; set; }
+
+        //Bug Not null currency :- can this Work ?  the String is ref type and can take null value 
+        public String Currency { get; set; }
+
+        [Column("Country_Code")]
+        public string CountryCode { get; set; }
+
+        [Column("State_Province_Code")]
+        public String StateProvinceCode { get; set; }
+
+        [Column("Street_Address")]
+        public string Street_Address { get; set; }
+
+        [Column("City_Town")] public String CityTown { get; set; }
+
+        [Column("Zip_Postal_Code")] public String ZipPostalCode { get; set; }
+
+        [Column("Time_Stamp")] public Byte[] TimeStamp { get; set; }
+
     }
 }
