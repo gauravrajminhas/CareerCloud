9bedc7ea26a90692d760b80ad5d7c1e6b0182b94
gauravrajminhas <gaurav.minhas@gmail.com>
2019-01-30 10:53:02 -0500

    Added Unit Test 4 to the project Removed Assignment 1,2,3 unit tests from the project

---
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using CareerCloud.Pocos;
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    class CareerCloudContext :DbContext
+    {
+        private static String connectionString = ConfigurationManager.ConnectionStrings["myHumberDB"].ConnectionString;
+
+        public CareerCloudContext() :base(connectionString)
+        {
+            
+        }
+
+        public DbSet ApplicantEducations { get; set; }
+        public DbSet ApplicantJobApplications { get; set; }
+        public DbSet ApplicantProfiles { get; set; }
+        public DbSet ApplicantResumes { get; set; }
+        public DbSet ApplicantSkills { get; set; }
+        public DbSet ApplicantWorkHistorys { get; set; }
+
+        public DbSet CompanyDescriptions { get; set; }
+        public DbSet CompanyJobDescriptions { get; set; }
+        public DbSet CompanyJobEducations { get; set; }
+        public DbSet CompanyJobs { get; set; }
+        public DbSet CompanyJobSkills { get; set; }
+        public DbSet CompanyLocations { get; set; }
+        public DbSet CompanyProfiles { get; set; }
+
+        public DbSet SecurityLogins { get; set; }
+        public DbSet SecurityLoginsLogs { get; set; }
+        public DbSet SecurityLoginsRoles { get; set; }
+        public DbSet SecurityRoles { get; set; }
+
+        public DbSet SystemCountryCodes { get; set; }
+        public DbSet SystemLanguageCodes { get; set; }
+
+       // public override void OnModelCreating(DbModelBuilder modelBuilder)
+        
+
+    }
+}
---
+++ CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    public class EFGenericRepository<SourceType> : IDataRepository<SourceType>
+        where SourceType : class
+    {
+        // CRUD operations but with generic implimentation 
+
+        public void Add(params SourceType[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SourceType> GetAll(params Expression<Func<SourceType, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SourceType> GetList(Expression<Func<SourceType, bool>> where, params Expression<Func<SourceType, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SourceType GetSingle(Expression<Func<SourceType, bool>> where, params Expression<Func<SourceType, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params SourceType[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params SourceType[] pocos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
