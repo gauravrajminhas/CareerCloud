cbe3c7e9dd376cef30e56a90f6eba2fa88ddf6ce
gauravrajminhas <gaurav.minhas@gmail.com>
2019-03-23 20:17:25 -0400

    dbsets virtual

--- CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -53,25 +53,25 @@ namespace CareerCloud.EntityFrameworkDataAccess
 
 
 
-        public DbSet ApplicantEducations { get; set; }
-        public DbSet ApplicantJobApplications { get; set; }
-        public DbSet ApplicantProfiles { get; set; }
-        public DbSet ApplicantResumes { get; set; }
-        public DbSet ApplicantSkills { get; set; }
-        public DbSet ApplicantWorkHistorys { get; set; }
+        public virtual DbSet ApplicantEducations { get; set; }
+        public virtual DbSet ApplicantJobApplications { get; set; }
+        public virtual DbSet ApplicantProfiles { get; set; }
+        public virtual DbSet ApplicantResumes { get; set; }
+        public virtual DbSet ApplicantSkills { get; set; }
+        public virtual DbSet ApplicantWorkHistorys { get; set; }
 
-        public DbSet CompanyDescriptions { get; set; }
-        public DbSet CompanyJobDescriptions { get; set; }
-        public DbSet CompanyJobEducations { get; set; }
-        public DbSet CompanyJobs { get; set; }
-        public DbSet CompanyJobSkills { get; set; }
-        public DbSet CompanyLocations { get; set; }
-        public DbSet CompanyProfiles { get; set; }
+        public virtual DbSet CompanyDescriptions { get; set; }
+        public virtual DbSet CompanyJobDescriptions { get; set; }
+        public virtual DbSet CompanyJobEducations { get; set; }
+        public virtual DbSet CompanyJobs { get; set; }
+        public virtual DbSet CompanyJobSkills { get; set; }
+        public virtual DbSet CompanyLocations { get; set; }
+        public virtual DbSet CompanyProfiles { get; set; }
 
-        public DbSet SecurityLogins { get; set; }
-        public DbSet SecurityLoginsLogs { get; set; }
-        public DbSet SecurityLoginsRoles { get; set; }
-        public DbSet SecurityRoles { get; set; }
+        public virtual DbSet SecurityLogins { get; set; }
+        public virtual DbSet SecurityLoginsLogs { get; set; }
+        public virtual DbSet SecurityLoginsRoles { get; set; }
+        public virtual DbSet SecurityRoles { get; set; }
 
         public DbSet SystemCountryCodes { get; set; }
         public DbSet SystemLanguageCodes { get; set; }
